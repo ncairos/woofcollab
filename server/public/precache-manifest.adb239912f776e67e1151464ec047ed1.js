f5c625dab097452169ef37e15bc953937e7a3b9c
Ninoska Cairos <ninocairos@NinoCairos.local>
2019-12-17 14:48:23 +0100

    Test OK

---
+++ server/public/precache-manifest.adb239912f776e67e1151464ec047ed1.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5c4deb2739e7140dd7d6567e60e55ec7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1ef84e6a528440018ee0",
+    "url": "/static/css/2.3785f652.chunk.css"
+  },
+  {
+    "revision": "cdf6b31ef2768c7f7607",
+    "url": "/static/css/main.e2e9aadd.chunk.css"
+  },
+  {
+    "revision": "1ef84e6a528440018ee0",
+    "url": "/static/js/2.4de73383.chunk.js"
+  },
+  {
+    "revision": "a6f8b8bb7b4de84cb9fae689d6d857fb",
+    "url": "/static/js/2.4de73383.chunk.js.LICENSE"
+  },
+  {
+    "revision": "cdf6b31ef2768c7f7607",
+    "url": "/static/js/main.a2a7de08.chunk.js"
+  },
+  {
+    "revision": "0dad2f18b4187af6cd88",
+    "url": "/static/js/runtime-main.26d07929.js"
+  },
+  {
+    "revision": "e4ebac6eb2f3a284eccbbc7c922cc166",
+    "url": "/static/media/BG2.e4ebac6e.png"
+  }
+]);
